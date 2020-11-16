950044541fe3bf871fa995ac2fd0ed1518205ebf
TossapornT <74558397+TossapornT@users.noreply.github.com>
2020-11-17 00:19:13 +0700

    Create Exercise4_Tossaporn_T.py

---
+++ Exercise4_Tossaporn_T.py
@@ -0,0 +1,10 @@
+course1 = 20
+course2 = 10
+course3 = 20
+course4 = 10
+print("Your Score")
+print("------------------")
+print("Foundation English :", course1,"pt.")
+print("General Business :", course2,"pt.")
+print("Introduction to Computer Systems :", course3,"pt.")
+print("Computer Programming :", course4,"pt.")
